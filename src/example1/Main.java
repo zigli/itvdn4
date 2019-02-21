08a5a9e779ecb409017ab1a28dfbe74fbc154e5f
ilgiz <4ilgiz@gmail.com>
2019-02-21 08:43:57 +0300

    Enums are final and static

--- src/example1/Main.java
+++ src/example1/Main.java
@@ -1,5 +1,7 @@
 package example1;
 
+//Enums are final and static
+
 enum Day {
     MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
 }
@@ -9,6 +11,5 @@ public class Main {
     public static void main(String[] args) {
 	    Day today = Day.SATURDAY;
         System.out.println("Today is " + today);
-
     }
 }

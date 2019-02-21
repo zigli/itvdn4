98ec8c8911662826e7a3182917ea821cdec5a3a8
ilgiz <4ilgiz@gmail.com>
2019-02-21 08:26:36 +0300

    simple enum

--- src/example1/Main.java
+++ src/example1/Main.java
@@ -1,8 +1,14 @@
 package example1;
 
+enum Day {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Day today = Day.SATURDAY;
+        System.out.println("Today is " + today);
+
     }
 }
